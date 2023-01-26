91e48bd3bbe2c88d357d98f514af1cf25e72b72c
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 19:11:43 -0300

    feat: Adicionado Model Category

---
+++ src/models/Category.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  });
+
+  return Category;
+}
