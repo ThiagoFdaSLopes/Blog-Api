b86c2e005e49ba83603e74d3c0bccb4e59f08827
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 19:32:28 -0300

    feat: service para listar categorias

--- src/services/categorie.service.js
+++ src/services/categorie.service.js
@@ -5,6 +5,12 @@ const createCategory = async (name) => {
     return dataValues;
 };
 
+const getAllCategories = async () => {
+  const allCategories = await Category.findAll();
+  return allCategories;
+};
+
 module.exports = {
   createCategory,
+  getAllCategories,
 };
