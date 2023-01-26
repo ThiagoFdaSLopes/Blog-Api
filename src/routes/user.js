c3c516954ae3d9ffb7ee99d90536088475d70901
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 18:51:24 -0300

    feat: Criado rota para validar e pegar users

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { userController } = require('../controllers');
 const validateBodyNewUser = require('../middlewares/validateBodyNewUser');
+const validateToken = require('../middlewares/validateToken');
 
 const router = express.Router();
 
 router.post('/', validateBodyNewUser, userController.createUser);
+router.get('/', validateToken, userController.findAllUsers);
 
 module.exports = router;
