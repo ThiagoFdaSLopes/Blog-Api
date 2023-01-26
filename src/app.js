ecb92338238bf457e8deb4b10e3e898f7d6ed028
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 16:44:24 -0300

    feat: Indexado rota login no app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 // ...
+const { routeLogin } = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use('/login', routeLogin);
 
 // ...
 
