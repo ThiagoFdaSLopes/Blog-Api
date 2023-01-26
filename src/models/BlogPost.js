697115293a90faf01a4e1d6cbe44925519c64a03
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 19:46:31 -0300

    feat: Criado association

---
+++ src/models/BlogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+    userId: { type: DataTypes.INTEGER, foreignKey: true, field: 'user_id' },
+  }, {
+    timestamps: false,
+    tableName: 'blog_posts',
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'users' });
+  };
+
+  return BlogPost;
+}
