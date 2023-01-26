1c8be4876342d01603b10e811be0ee8c1d17f82f
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 16:43:22 -0300

    fix: Comentado associate da model user

--- src/models/User.js
+++ src/models/User.js
@@ -14,10 +14,10 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
-  User.associate = (models) => {
-    User.hasMany(models.BlogPost, 
-    { foreignKey: 'userId', as: 'blogposts'});
-  };
+  // User.associate = (models) => {
+  //   User.hasMany(models.BlogPost, 
+  //   { foreignKey: 'userId', as: 'blogposts'});
+  // };
 
   return User;
 }
