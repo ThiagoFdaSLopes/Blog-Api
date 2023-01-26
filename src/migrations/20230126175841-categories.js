142bd4a20aad50f32fb1e9ac8a59df708302c164
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 15:14:43 -0300

    feat: Criado Migrate para categories

---
+++ src/migrations/20230126175841-categories.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: { 
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('categories')
+  }
+};
+
