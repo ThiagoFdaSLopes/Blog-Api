2a4f866dd214223c73786d359713b49b72e38b87
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 16:53:25 -0300

    fix: Corrigido unique no migrate

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
-    email: {
-     type: DataTypes.STRING,
-     unique: true,
-    },
+    email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
   }, {
