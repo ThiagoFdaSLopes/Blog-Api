de580fccf9e974752966b64dc7a49c93f4cdc16a
Thiago <programadorthiagolopes@gmail.com>
2023-01-26 15:35:19 -0300

    feat: Criado model User

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: {
+     type: DataTypes.STRING,
+     unique: true,
+    },
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, 
+    { foreignKey: 'userId', as: 'blogposts'});
+  };
+
+  return User;
+}
