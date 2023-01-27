d124a182e1126ecc60f58ee6fca5c76d410ff13f
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 09:27:57 -0300

    fix: Alterado logica do validateToken

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,11 +1,19 @@
-const { validateToken } = require('../utils/token');
+const { validateToken, verifyToken } = require('../utils/token');
+const userService = require('../services/user.service');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) return res.status(401).json({ message: 'Token not found' });
 
   const { type, message } = validateToken(authorization);
   if (type) return res.status(401).json({ message });
 
+  const decoded = verifyToken(authorization);
+
+  const user = await userService.findUserByEmail(decoded.email);
+
+  if (!user) return res.status(400).json({ message: 'User Not Found' });
+
+  req.user = user;
   next();
 };
