172b4dd467b089d4fb15dde172972ea7634b8ce9
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 09:55:55 -0300

    feat: Criado rotas para listar por id e all

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,5 +6,7 @@ const validateBodyCreatePost = require('../middlewares/validateBodyCreatePost');
 const router = express.Router();
 
 router.post('/', validateToken, validateBodyCreatePost, postCategoryController.createPost);
+router.get('/', validateToken, postCategoryController.findAllPosts);
+router.get('/:id', validateToken, postCategoryController.findPostByid);
 
 module.exports = router;
