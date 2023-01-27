1e27c99c5b0a3c02d015c818856b5b6cd0910478
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 11:33:27 -0300

    feat: validateBodyUpdate

---
+++ src/middlewares/validateBodyUpdate.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) { 
+    return res.status(400).json({ message: 'Some required fields are missing' }); 
+  }
+
+  next();
+};
