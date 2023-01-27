95b4e136f1f0aa5ba57b024cb488947f5d3b2e30
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 09:28:49 -0300

    feat: newService para post

--- src/migrations/20230126180056-blog_posts.js
+++ src/migrations/20230126180056-blog_posts.js
@@ -19,11 +19,11 @@ module.exports = {
       },
       published: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       userId: {
         type: Sequelize.INTEGER,
