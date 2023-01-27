5b470f510e819a4a0f42b66d9775569352477c2e
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 09:55:41 -0300

    feat: Criado controller para listar por id e all

--- src/controllers/postcategory.controller.js
+++ src/controllers/postcategory.controller.js
@@ -10,6 +10,24 @@ const createPost = async (req, res) => {
   res.status(201).json(message);
 };
 
+const findPostByid = async (req, res) => {
+  const { id } = req.params;
+
+  const { type, message } = await postService.findPostByid(id);
+
+  if (type) return res.status(404).json({ message });
+
+  res.status(200).json(message);
+};
+
+const findAllPosts = async (_req, res) => {
+  const posts = await postService.getAllPosts();
+
+  res.status(200).json(posts);
+};
+
 module.exports = {
   createPost,
+  findPostByid,
+  findAllPosts,
 };
