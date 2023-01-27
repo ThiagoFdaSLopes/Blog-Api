092fd174941bfbd1f60be1634fa6945b4951091c
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 11:33:39 -0300

    feat: rotas para update

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { postCategoryController } = require('../controllers');
 const validateToken = require('../middlewares/validateToken');
 const validateBodyCreatePost = require('../middlewares/validateBodyCreatePost');
+const validateBodyUpdate = require('../middlewares/validateBodyUpdate');
 
 const router = express.Router();
 
 router.post('/', validateToken, validateBodyCreatePost, postCategoryController.createPost);
+router.put('/:id', validateToken, validateBodyUpdate, postCategoryController.updatePost);
 router.get('/', validateToken, postCategoryController.findAllPosts);
 router.get('/:id', validateToken, postCategoryController.findPostByid);
 
