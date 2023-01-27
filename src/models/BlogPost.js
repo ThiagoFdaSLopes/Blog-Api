a9ed52d976ab5b41825458c89ed66cf358f69e97
Thiago <programadorthiagolopes@gmail.com>
2023-01-27 09:56:07 -0300

    fix: Alias alterado para user

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
 
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User,
-      { foreignKey: 'userId', as: 'users' });
+      { foreignKey: 'userId', as: 'user' });
   };
 
   return BlogPost;
